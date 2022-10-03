18f54410cc269b153a11beaefe1e5a5c88bb2731
LAB07nakasima <rumor0301@icloud.com>
2022-10-04 03:58:26 +0900

    CSS修正3

--- src/resources/views/post/show.blade.php
+++ src/resources/views/post/show.blade.php
@@ -173,11 +173,11 @@
             </div>
 
             <!--Divider(区切り)-->
-            <hr class="border-b-2 border-gray-400 mb-8 mx-4">
+            {{-- <hr class="border-b-2 border-gray-400 mb-8 mx-4"> --}}
 
             <form action="{{ route('comment.store') }}" method="POST">
                 @csrf
-                <div class="px-5 py-24 mx-auto flex justify-center">
+                <div class="px-5 py-8 mx-auto flex justify-center">
                     <div class="lg:w-full md:w-full p-8 flex flex-col w-full mt-10 md:mt-0">
                         <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">コメントする</h2>
                         <p class="leading-relaxed mb-5 text-gray-600">気になったことや質問、応援など書いてください</p>
